094eb9838f732cec0e6b7dc7d212938790855d1d
Vitor Telles <vitortelles120599@gmail.com>
2021-10-19 22:20:14 -0300

    primeiro import

---
+++ painel/pages/visualizar-pagamentos.php
@@ -0,0 +1,37 @@
+
+<div class="box-content">
+<h2><i class="fa fa-id-card-o"></i> Pagamentos Pendentes</h2>
+
+	<div class="wraper-table">
+	<table>
+		<tr>
+			<td>Nome do pagamento</td>
+			<td>Cliente</td>
+			<td>Valor</td>
+			<td>Vencimento</td>
+			<td>#</td>
+		</tr>
+
+
+	</table>
+	</div>
+
+	<h2><i class="fa fa-id-card-o"></i> Pagamentos Concluídos</h2>
+
+	<div class="wraper-table">
+	<table>
+		<tr>
+			<td>Nome do pagamento</td>
+			<td>Cliente</td>
+			<td>Valor</td>
+			<td>Vencimento</td>
+			<td>#</td>
+		</tr>
+
+
+	</table>
+	</div>
+
+
+
+</div><!--box-content-->
